9c97b693d291bcce53b5c18a71d45fc2bced28da
Eryk Halicki <35389308+Stargor14@users.noreply.github.com>
2021-02-01 19:05:42 -0500

    Update cryptochart.py

--- cryptochart.py
+++ cryptochart.py
@@ -26,8 +26,8 @@ row = 4
 global date
 date = datetime.date.today()
 
-apikey = "x15G8QtfrUCJG1F7tCahCwyCwxE7a3Mbykg8Q4Uf0Q7QKjB1B3GvCYkfzRUTS96e"
-secretkey = "YyATCoS7OwFFAdaqK3UCw0zZLZoz6RWRqarMrHhGi7P08c7Muay8zDWZfV86SxA5"
+apikey = #enter public api key
+secretkey = #enter private key
 
 def req():
     global row
